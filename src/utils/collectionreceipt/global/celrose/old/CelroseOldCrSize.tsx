7dcc49afdc2e1739e370ff22d2b580d3b9028032
smctdev <168695959+smctdev@users.noreply.github.com>
2025-03-18 11:05:05 +0800

    adjusted pl and ml of cr-celrose

--- src/utils/collectionreceipt/global/celrose/old/CelroseOldCrSize.tsx
+++ src/utils/collectionreceipt/global/celrose/old/CelroseOldCrSize.tsx
@@ -16,13 +16,13 @@ const CelroseOldCrSize: React.FC<PrintPageProps> = ({ data }) => {
   return (
     <div className="text-xs w-[805.03937008px] h-[415.7480315px]">
       <div className="flex mt-[95.1732283431px]">
-        <p className="pl-[620.07874016px] w-[143.62204724px]">
+        <p className="pl-[582.28346457px] w-[143.62204724px]">
           {data[1]?.[CR_Date] || <span className="opacity-0">No data</span>}
         </p>
       </div>
-      <div className="flex justify-between mt-[24.7952755906px]">
+      <div className="flex justify-between mt-[9.6771653556px]">
         <div className="w-[574.48818898px]">
-          <p className="pl-[321.25984252px]">
+          <p className="pl-[283.46456693px]">
             {data[1]?.[CR_Name] || <span className="opacity-0">No data</span>}
           </p>
         </div>
@@ -32,42 +32,42 @@ const CelroseOldCrSize: React.FC<PrintPageProps> = ({ data }) => {
           </p>
         </div>
       </div>
-      <div className="flex mt-[6.8267716539px]">
-        <p className="pl-[313.7007874px]">
+      <div className="flex">
+        <p className="pl-[275.90551181px]">
           {data[1]?.[CR_Address] || <span className="opacity-0">No data</span>}
         </p>
       </div>
-      <div className="flex mt-[3.8267716539px]">
-        <p className="ml-[306.14173228px]">
+      <div className="flex">
+        <p className="ml-[268.34645669px]">
           {data[1]?.[CR_BusinessStyle] || (
             <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
-      <div className="flex w-full mt-[3.8267716539px]">
+      <div className="flex w-full">
         <div className="w-[600.94488189px]">
-          <p className="pl-[249.4488189px]">
+          <p className="pl-[211.65354331px]">
             {data[1]?.[CR_AmountInWords] || (
               <span className="opacity-0">No data</span>
             )}
           </p>
         </div>
         <div className="w-[181.41732283px]">
-          <p className="pl-[52.913385827px]">
+          <p className="pl-[37.795275591px]">
             {data[1]?.[CR_AmountInFigures] || (
               <span className="opacity-0">No data</span>
             )}
           </p>
         </div>
       </div>
-      <div className="w-[782.36220472px] mt-[7.8267716539px]">
-        <p className="w-[419.52755906px] ml-[347.71653543px]">
+      <div className="w-[782.36220472px]">
+        <p className="w-[419.52755906px] ml-[309.92125984px]">
           {data[1]?.[CR_Memo].substring(0, 70) || (
             <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
-      <div className="w-[782.36220472px] mt-[6.8267716539px]">
+      <div className="w-[782.36220472px]">
         <p className="w-[521.57480315px] ml-[245.66929134px]">
           {data[1]?.[CR_Memo].substring(70) || (
             <span className="opacity-0">No data</span>
@@ -75,7 +75,7 @@ const CelroseOldCrSize: React.FC<PrintPageProps> = ({ data }) => {
         </p>
       </div>
 
-      <div className="w-[782.36220472px] pl-[566.92913386px] mt-[28.7952755906px] text-[11px]">
+      <div className="w-[782.36220472px] pl-[529.13385827px] mt-[9.8976377953px] text-[11px]">
         <p className="text-center w-[170.07874016px]">
           {data[1]?.[CR_PartnerName] || (
             <span className="opacity-0">No data</span>
