0ccdb8764c1a976a733f6415f5cfe5fb56507a3a
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-03-19 16:50:07 +0800

    Grayn and rayd

--- src/utils/printLabel.tsx
+++ src/utils/printLabel.tsx
@@ -3,13 +3,13 @@ import { FaXmark, FaCheckDouble } from "react-icons/fa6";
 export const printLabel = (title: any, isLabel: any) => {
   if (isLabel) {
     return (
-      <span className="flex gap-1 text-green-600 items-center">
+      <span className="flex gap-1 items-center" style={{ color: "green" }}>
         <FaCheckDouble /> {title}
       </span>
     );
   } else {
     return (
-      <span className="flex gap-1 items-center">
+      <span className="flex gap-1 text-red-600 items-center">
         <FaXmark /> {title}
       </span>
     );
