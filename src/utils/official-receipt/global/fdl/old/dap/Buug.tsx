fe5cde9aa211e2130c78afa2bdc63c37036c0bc0
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-03-24 16:56:12 +0800

    adjusted

--- src/utils/official-receipt/global/fdl/old/dap/Buug.tsx
+++ src/utils/official-receipt/global/fdl/old/dap/Buug.tsx
@@ -16,52 +16,52 @@ const Buug = ({ data }: any) => {
 
   return (
     <div className="text-xs w-[585.82677165px] h-[343.93700787px] ml-[10px]">
-      <div className="flex mt-[101.38582677px]">
-        <p className="ml-[472.44094488px] w-[147.4015748px]">
+      <div className="flex mt-[75.590551181px]">
+        <p className="ml-[457.32283465px] w-[105.82677165px]">
           {data[1]?.[CR_Date] || <span className="opacity-0">No data</span>}
         </p>
       </div>
-      <div className="flex w-[585.82677165px] mt-[26.7952755906px]">
-        <div className="w-[442.20472441px]">
-          <p className="ml-[264.56692913px]">
+      <div className="flex w-[585.82677165px] mt-[15.456692912px]">
+        <div className="w-[453.54330709px]">
+          <p className="ml-[260.78740157px]">
             {data[1]?.[CR_Name] || <span className="opacity-0">No data</span>}
           </p>
         </div>
-        <div className="w-[139.08661417px]">
-          <p className="ml-[49.133858268px]">
+        <div className="w-[136.44094488px]">
+          <p className={`ml-[49.133858268px] ${data[1]?.[CR_TIN]?.length > 10 ? 'text-[9.5px]' : ''}`}>
             {data[1]?.[CR_TIN] || <span className="opacity-0">No data</span>}
           </p>
         </div>
-      </div>
+      </div> 
       <div className="flex mt-[9.8267716539px]">
-        <p className={`${data[1]?.[CR_Address].length > 28 ? 'text-[11px]' : ''} ml-[272.12598425px] w-[381.73228346px]`}>
+        <p className={`${data[1]?.[CR_Address]?.length > 28 ? 'text-[9.5px]' : ''} ml-[257.00787402px] w-[251.33858268px]`}>
           {data[1]?.[CR_Address] || <span className="opacity-0">No data</span>}
         </p>
       </div>
       <div className="flex mt-[6.8267716539px] w-[585.82677165px]">
-        <p className={`${data[1]?.[CR_BusinessStyle].length > 28 ? 'text-[11px]' : ''} ml-[279.12598425px] w-[381.73228346px]`}>
+        <p className={`${data[1]?.[CR_BusinessStyle]?.length > 28 ? 'text-[9.5px]' : ''} ml-[263.00787402px] w-[251.33858268px]`}>
           {data[1]?.[CR_BusinessStyle] || (
             <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
-      <div className="flex mt-[3.8267716539px]">
-        <p className="ml-[226.77165354px] w-[472.44094488px]">
+      <div className="flex mt-[3.8267716539px] w-full">
+        <p className={`ml-[196.53543307px] w-[347.71653543px] ${data[1]?.[CR_AmountInWords]?.length > 28 ? "text-[9.5px]" : "" }`}>
           {data[1]?.[CR_AmountInWords] || (
             <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
       <div className="flex w-full mt-[6.8267716539px]">
-        <div className="w-[325.03937008px]">
-          <p className="ml-[245.66929134px]">
+        <div className="w-[268.34645669px]">
+          <p className="ml-[204.09448819px]">
             {FormattedNumber(data[1]?.[CR_AmountInFigures]) || (
               <span className="opacity-0">No data</span>
             )}
           </p>
         </div>
-        <div className="w-[274.01574803px] ml-[132.28346457px]">
-          <p>
+        <div className="w-[298.58267717px]">
+          <p className={`${data[1]?.[CR_Memo]?.length > 28 ? 'text-[9.5px]' : ''} ml-[105.82677165px]`}>
             {data[1]?.[CR_Memo] || (
               <span className="opacity-0">No data</span>
             )}
@@ -69,7 +69,7 @@ const Buug = ({ data }: any) => {
         </div>
       </div>
       <div className="mt-[25.7952755906px]">
-        <p className="w-[577.88976378px] pl-[445.98425197px] mt-[6.7952755906px] text-[11px]">
+        <p className="w-[577.88976378px] pl-[445.98425197px] mt-[56.692913386px] text-[11px]">
           {data[1]?.[CR_PartnerName] || (
             <span className="opacity-0">No Data</span>
           )}
