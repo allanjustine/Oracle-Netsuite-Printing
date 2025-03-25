e9e9bad6c339456e19200b7d1019abb4fb41b230
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-03-25 10:06:43 +0800

    Adjusted Panabo

--- src/utils/salesinvoice/global/obbus/old/dsm/Panabo.tsx
+++ src/utils/salesinvoice/global/obbus/old/dsm/Panabo.tsx
@@ -112,7 +112,7 @@ const Panabo = ({ data }: any) => {
 
   return (
     <div className="text-xs h-[795.67716535px] w-[616.06299213px]">
-      <div className="flex h-[15.458267717px] mt-[116.1496063px]">
+      <div className="flex h-[15.458267717px] mt-[75.866929134px]">
         <p className="w-[402.51968504px] ml-[86.929133858px]">
           {data[1]?.[mainLineName]
             ?.replace(/Ã/g, "Ñ")
