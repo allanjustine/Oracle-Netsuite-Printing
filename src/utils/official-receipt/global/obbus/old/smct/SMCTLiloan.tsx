5f07126520bb9adc92ceea8d5d4041558c56bde1
smctdev <168695959+smctdev@users.noreply.github.com>
2025-03-25 08:46:35 +0800

    adjusted top of or of smct liloan

--- src/utils/official-receipt/global/obbus/old/smct/SMCTLiloan.tsx
+++ src/utils/official-receipt/global/obbus/old/smct/SMCTLiloan.tsx
@@ -16,7 +16,7 @@ const SMCTLiloan = ({ data }: any) => {
 
   return (
     <div className="text-xs w-[801.25984252px] h-[408.18897638px] ml-[10px]">
-      <div className="flex mt-[111.38582677px]">
+      <div className="flex mt-[128.50393701px]">
         <p className="ml-[659.72440945px] w-[151.18110236px]">
           {data[1]?.[CR_Date] || <span className="opacity-0">No data</span>}
         </p>
