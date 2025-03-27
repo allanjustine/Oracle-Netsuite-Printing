e2fb057cd1729773f8ed6bcb1a161017a253f0ef
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-03-27 16:03:01 +0800

    t

---
+++ src/utils/official-receipt/global/fely/old/smct/Dasmariñas.tsx
@@ -0,0 +1,89 @@
+"use client";
+import FormattedNumber from "@/utils/FormattedNumber";
+
+const Dasmariñas = ({ data }: any) => {
+  const CR_Date = 0;
+  const CR_Name = 1;
+  const CR_TIN = 2;
+  const CR_Address = 3;
+  const CR_BusinessStyle = 4;
+  const CR_AmountInFigures = 5;
+  const CR_AmountInWords = 6;
+  const CR_Memo = 7;
+  const CR_FormOfPayment = 8;
+  const CR_PartnerName = 9;
+  const CR_Reference = 10;
+
+  return (
+    <div className="text-xs w-[608.50393701px] h-[404.40944882px]">
+      <div className="flex mt-[137.4015748px]">
+        <p className="ml-[491.33858268px] w-[94.488188976px] h-[22.677165354px]">
+          {data[1]?.[CR_Date] || <span className="opacity-0">No data</span>}
+        </p>
+      </div>
+      <div className="flex justify-between mt-[7.7952755906px]">
+        <div className="w-[491.33858268px]">
+          <p className="ml-[309.92125984px] h-[22.677165354px]">
+            {data[1]?.[CR_Name] || <span className="opacity-0">No data</span>}
+          </p>
+        </div>
+        <div className="w-[117.16535433px]">
+          <p className="pl-[45.354330709px] h-[22.677165354px]">
+            {data[1]?.[CR_TIN] || <span className="opacity-0">No data</span>}
+          </p>
+        </div>
+      </div>
+      <div className="flex">
+        <p className="ml-[291.02362205px] w-[226.77165354px] h-[22.677165354px]">
+          {data[1]?.[CR_Address] || <span className="opacity-0">No data</span>}
+        </p>
+      </div>
+      <div className="flex">
+        <p className="ml-[291.02362205px] w-[241.88976378px] h-[22.677165354px]">
+          {data[1]?.[CR_BusinessStyle] || (
+            <span className="opacity-0">No data</span>
+          )}
+        </p>
+      </div>
+      <div className="flex">
+        <p className="ml-[245.77165354px] h-[22.677165354px]">
+          {data[1]?.[CR_AmountInWords] || (
+            <span className="opacity-0">No data</span>
+          )}
+        </p>
+      </div>
+      <div className="flex w-full">
+        <div className="w-[325.03937008px]">
+          <p className="ml-[254.66929134px] h-[22.677165354px]">
+            {FormattedNumber(data[1]?.[CR_AmountInFigures]) || (
+              <span className="opacity-0">No data</span>
+            )}
+          </p>
+        </div>
+        <div className="w-[274.01574803px] ml-[158.28346457px]">
+          <p className="h-[22.677165354px]">
+            {data[1]?.[CR_Memo].substring(0, 35) || (
+              <span className="opacity-0">No data</span>
+            )}
+          </p>
+        </div>
+      </div>
+      <div className="ml-[245.77165354px]">
+        <p className="h-[22.677165354px]">
+          {data[1]?.[CR_Memo].substring(35) || (
+            <span className="opacity-0">No data</span>
+          )}
+        </p>
+      </div>
+      <div className="w-[718.11023622px] pl-[566.92913386px] mt-[27.7952755906px]">
+        <p className="h-[22.677165354px]">
+          {data[1]?.[CR_PartnerName] || (
+            <span className="opacity-0">No Data</span>
+          )}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Dasmariñas;
