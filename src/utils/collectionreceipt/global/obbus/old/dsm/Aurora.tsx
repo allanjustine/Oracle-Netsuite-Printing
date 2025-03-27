14a847bae941c40c71f3d25fec9818cfc2cabf1b
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-03-27 09:01:18 +0800

    Cr adjusted

--- src/utils/collectionreceipt/global/obbus/old/dsm/Aurora.tsx
+++ src/utils/collectionreceipt/global/obbus/old/dsm/Aurora.tsx
@@ -16,59 +16,62 @@ const Aurora: React.FC<PrintPageProps> = ({ data }) => {
   const CR_Reference = 10;
 
   return (
-    <div className="text-xs w-[752.12598425px] h-[389.29133858px] ml-[86.133858268px]">
-      <div className="flex mt-[123.8503937px]">
-        <p className="pl-[649.72440945px] w-[143.62204724px]">
+    <div className="text-xs w-[805.03937008px] h-[415.7480315px]">
+      <div className="flex mt-[105px]">
+        <p className="pl-[642.51968504px] w-[120.94488189px]">
           {data[1]?.[CR_Date] || <span className="opacity-0">No data</span>}
         </p>
       </div>
       <div className="flex justify-between  mt-[15.7952755906px]">
-        <div className="w-[566.92913386px]">
-          <p className="pl-[316.7007874px]">
+        <div className="w-[595.27559055px]">
+          <p className="pl-[321.25984252px] h-[20.299212598px]">
             {data[1]?.[CR_Name] || <span className="opacity-0">No data</span>}
           </p>
         </div>
-        <div className="w-[202.58267717px]">
-          <p className="pl-[71.795275591px]">
+        <div className="w-[209.76377953px]">
+          <p className="pl-[49.133858268px] h-[20.299212598px]">
             {data[1]?.[CR_TIN] || <span className="opacity-0">No data</span>}
           </p>
         </div>
       </div>
       <div className="flex mt-[3.8267716539px]">
-        <p className="pl-[312.36220472px]">
+        <p className="ml-[313.7007874px] w-[415.7480315px] h-[20.299212598px]">
           {data[1]?.[CR_Address] || <span className="opacity-0">No data</span>}
         </p>
       </div>
-      <div className="flex mt-[6.8267716539px]">
-        <p className="ml-[302.80314961px]">
+      <div className="flex">
+        <p className="ml-[302.36220472px] w-[415.7480315px] h-[20.299212598px]">
           {data[1]?.[CR_BusinessStyle] || (
             <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
-      <div className="flex w-full mt-[3.8267716539px]">
-        <div className="w-[604.72440945px]">
-          <p className="pl-[239.33070866px]">
+      <div className="flex w-full">
+        <div className="w-[606.61417323px]">
+          <p className="pl-[238.11023622px] h-[20.299212598px]">
             {data[1]?.[CR_AmountInWords] || (
               <span className="opacity-0">No data</span>
             )}
           </p>
         </div>
-        <div className="w-[164.40944882px]">
-          <p className="pl-[60.354330709px]">
+        <div className="w-[198.42519685px]">
+          <p className="pl-[49.133858268px] h-[20.299212598px]">
             {FormattedNumber(data[1]?.[CR_AmountInFigures]) || (
               <span className="opacity-0">No data</span>
             )}
           </p>
         </div>
       </div>
-      <div className="w-[529px] ml-[350.15748031px] mt-[6.8267716539px]">
-        <p>
-          {data[1]?.[CR_Memo] || <span className="opacity-0">No data</span>}
+      <div className="w-full h-[41.57480315px]">
+        <p className="ml-[350.15748031px] w-[423.30708661px] h-[20.299212598px]">
+          {data[1]?.[CR_Memo]?.substring(0, 70) || <span className="opacity-0">No data</span>}
+        </p>
+        <p className="ml-[241.88976378px] w-[521.57480315] h-[20.299212598px]">
+          {data[1]?.[CR_Memo]?.substring(70) || <span className="opacity-0">No data</span>}
         </p>
       </div>
       
-      <div className="w-[769.13385827px] pl-[566.92913386px] mt-[45.7952755906px] text-[11px]">
+      <div className="w-[769.13385827px] pl-[566.92913386px] mt-[26.456692913px] text-[11px]">
         <p>
         {data[1]?.[CR_PartnerName] || ""}
         </p>
