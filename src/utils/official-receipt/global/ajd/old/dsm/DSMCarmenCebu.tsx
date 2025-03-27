61f49324c8add5ca2cd0b2e362bc3df428035b7b
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-03-27 10:58:47 +0800

    Carmen Cebu adjusted

--- src/utils/official-receipt/global/ajd/old/dsm/DSMCarmenCebu.tsx
+++ src/utils/official-receipt/global/ajd/old/dsm/DSMCarmenCebu.tsx
@@ -15,60 +15,56 @@ const DSMCarmenCebu = ({ data }: any) => {
   const CR_Reference = 10;
 
   return (
-    <div className="text-xs w-[778.58267717px] h-[396.8503937px] ml-[10px]">
-      <div className="flex mt-[96.708661417px]">
-        <p className="ml-[491.33858268px] w-[102.04724409px]">
-          {data[1]?.[CR_Date] || <span className="opacity-0">No data</span>}
-        </p>
-      </div>
-      <div className="flex w-[778.58267717px] mt-[13.7952755906px]">
-        <div className="w-[461.1023622px]">
-          <p className="ml-[313.7007874px] text-xs">
+    <div className="text-xs w-[628.15748031px] h-[393.07086614px]">
+      <p className="ml-[480px] mt-[104.26771654px] w-[105.82677165px] h-[22.677165354px]">
+        {data[1]?.[CR_Date] || <span className="opacity-0">No data</span>}
+      </p>
+      <div className="flex w-[628.15748031px] mt-[4.5354330709px]">
+        <div className="w-[468.66141732px]">
+          <p className="ml-[309.92125984px] text-xs h-[22.677165354px]">
             {data[1]?.[CR_Name] || <span className="opacity-0">No data</span>}
           </p>
         </div>
-        <div className="w-[156.47244094px]">
-          <p className="ml-[56.692913386px]">
+        <div className="w-[159.49606299px]">
+          <p className="ml-[52.913385827px] h-[22.677165354px]">
             {data[1]?.[CR_TIN] || <span className="opacity-0">No data</span>}
           </p>
         </div>
       </div>
-      <div className="flex ml-[302.36220472px] mt-[9.8267716539px]">
-        <p className="w-[211.65354331px] text-xs">
+      <div className="flex ml-[294.80314961px]">
+        <p className={`w-[226.77165354px] h-[22.677165354px] ${data[1]?.[CR_Address]?.length > 28 ? "text-[9.5px]" : "text-xs"}`}>
           {data[1]?.[CR_Address] || <span className="opacity-0">No data</span>}
         </p>
       </div>
-      <div className="flex text-xs ml-[317.48031496px] mt-[6.8267716539px]">
-        <p className="w-[207.87401575px]">
+      <div className="flex text-xs ml-[313.7007874px]">
+        <p className="w-[226.77165354px] h-[22.677165354px]">
           {data[1]?.[CR_BusinessStyle] || (
             <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
-      <div className="flex ml-[226.77165354px] mt-[3.8267716539px]">
-        <p className="w-[328.81889764px]">
+      <div className="flex ml-[219.21259843px]">
+        <p className="w-[340.15748031px] h-[22.677165354px]">
           {data[1]?.[CR_AmountInWords] || (
             <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
-      <div className="flex w-[778.58267717px] mt-[6.8267716539px]">
-        <div className="w-[325.03937008px]">
-          <p className="ml-[234.33070866px]">
+      <div className="flex w-[628.15748031px]">
+        <div className="w-[332.5984252px]">
+          <p className="ml-[234.33070866px] h-[22.677165354px]">
             {FormattedNumber(data[1]?.[CR_AmountInFigures]) || (
               <span className="opacity-0">No data</span>
             )}
           </p>
         </div>
-        <div className="w-[292.91338583px]">
-          <p className="ml-[147.4015748px] text-xs">
-            {data[1]?.[CR_Memo] || (
-              <span className="opacity-0">No data</span>
-            )}
+        <div className="w-[253.22834646px]">
+          <p className={`ml-[145.18110236px] ${data[1]?.[CR_Memo]?.length > 28 ? "text-[9.5px]" : "text-xs"} h-[22.677165354px]`}>
+            {data[1]?.[CR_Memo] || <span className="opacity-0">No data</span>}
           </p>
         </div>
       </div>
-      <div className="w-[188.97637795px] ml-[404.40944882px] mt-[52.913385827px]">
+      <div className="w-[196.53543307px] ml-[408.18897638px] mt-[43.913385827px]">
         <p className="text-center">
           {data[1]?.[CR_PartnerName] || (
             <span className="opacity-0">No Data</span>
