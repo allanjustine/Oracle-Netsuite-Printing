79b6dc387907faab5c59162c6866cf15dcf4d63b
Mark Anthony Bulala <117882831+macmac-prog@users.noreply.github.com>
2025-03-28 10:38:29 +0800

    Update HDKabasalan.tsx

--- src/utils/official-receipt/global/fdl/old/hd/HDKabasalan.tsx
+++ src/utils/official-receipt/global/fdl/old/hd/HDKabasalan.tsx
@@ -15,52 +15,52 @@ const HDKabasalan = ({ data }: any) => {
   const CR_Reference = 10;
 
   return (
-    <div className="text-xs w-[755.90551181px] h-[404.03149606px] ml-[-27px]">
-      <div className="flex mt-[101.38582677px]">
-        <p className="ml-[585.82677165px] w-[147.4015748px]">
+    <div className="text-xs w-[608.50393701px] h-[381.73228346px]">
+      <div className="flex mt-[90.38582677px]">
+        <p className="ml-[491.33858268px] w-[113.38582677px]">
           {data[1]?.[CR_Date] || <span className="opacity-0">No data</span>}
         </p>
       </div>
-      <div className="flex w-[755.90551181px] mt-[26.7952755906px]">
-        <div className="w-[578.26771654px]">
-          <p className="ml-[313.7007874px]">
+      <div className="flex w-[608.50393701px] mt-[15.7952755906px]">
+        <div className="w-[472.44094488px]">
+          <p className="ml-[283.46456693px]">
             {data[1]?.[CR_Name] || <span className="opacity-0">No data</span>}
           </p>
         </div>
-        <div className="w-[177.63779528px]">
-          <p className="ml-[30.236220472px]">
+        <div className="w-[113.38582677px]">
+          <p className="ml-[37.795275591px]">
             {data[1]?.[CR_TIN] || <span className="opacity-0">No data</span>}
           </p>
         </div>
       </div>
-      <div className="flex ml-[302.36220472px] mt-[9.8267716539px]">
-        <p className="w-[362.83464567px] text-[11px]">
+      <div className="flex ml-[264.56692913px] mt-[9.8267716539px]">
+        <p className="w-[245.66929134px] text-[11px]">
           {data[1]?.[CR_Address] || <span className="opacity-0">No data</span>}
         </p>
       </div>
-      <div className="flex mt-[6.8267716539px] w-[755.90551181px]">
-        <p className="ml-[294.80314961px] w-[381.73228346px] text-xs">
+      <div className="flex mt-[6.8267716539px] w-[608.50393701px]">
+        <p className="ml-[283.46456693px] w-[264.56692913px] text-xs">
           {data[1]?.[CR_BusinessStyle] || (
             <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
       <div className="flex mt-[3.8267716539px]">
-        <p className="ml-[226.77165354px] w-[472.44094488px]">
+        <p className="ml-[207.87401575px] w-[359.05511811px]">
           {data[1]?.[CR_AmountInWords] || (
             <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
       <div className="flex w-full mt-[6.8267716539px]">
-        <div className="w-[325.03937008px]">
-          <p className="ml-[245.66929134px]">
+        <div className="w-[302.36220472px]">
+          <p className="ml-[226.77165354px]">
             {FormattedNumber(data[1]?.[CR_AmountInFigures]) || (
               <span className="opacity-0">No data</span>
             )}
           </p>
         </div>
-        <div className="w-[274.01574803px] ml-[132.28346457px]">
+        <div className="w-[170.07874016px] ml-[94.488188976px]">
           <p>
             {data[1]?.[CR_Memo].substring(0, 35) || (
               <span className="opacity-0">No data</span>
@@ -75,8 +75,8 @@ const HDKabasalan = ({ data }: any) => {
           )}
         </p>
       </div>
-      <div className="w-[718.11023622px] mt-[25.7952755906px]">
-        <p className="ml-[566.92913386px]">
+      <div className="w-[585.82677165px] mt-[25.7952755906px]">
+        <p className="ml-[434.64566929px]">
           {data[1]?.[CR_PartnerName] || (
             <span className="opacity-0">No Data</span>
           )}
