d575d4e0835e0a3c43da183db00031effca2eebc
Mark Anthony Bulala <117882831+macmac-prog@users.noreply.github.com>
2025-04-15 10:53:44 +0800

    Individual size for DSM Toledo 2

---
+++ src/utils/official-receipt/global/obbus/old/dsm/DSMToledo2.tsx
@@ -0,0 +1,90 @@
+"use client";
+import FormattedNumber from "@/utils/FormattedNumber";
+import enyeFormat from "@/utils/enyeFormat";
+
+const DSMToledo2 = ({ data }: any) => {
+  const CR_Date = 0;
+  const CR_Name = 1;
+  const CR_TIN = 2;
+  const CR_Address = 3;
+  const CR_BusinessStyle = 4;
+  const CR_AmountInFigures = 5;
+  const CR_AmountInWords = 6;
+  const CR_Memo = 7;
+  const CR_FormOfPayment = 8;
+  const CR_PartnerName = 9;
+  const CR_Reference = 10;
+
+  return (
+    <div className="text-xs w-[801.25984252px] h-[408.18897638px] ml-[10px]">
+      <div className="flex mt-[96.38582677px]">
+        <p className="ml-[659.72440945px] w-[151.18110236px]">
+          {data[1]?.[CR_Date] || <span className="opacity-0">No data</span>}
+        </p>
+      </div>
+      <div className="flex justify-between  mt-[15.7952755906px]">
+        <div className="w-[566.92913386px]">
+          <p className="ml-[317.48031496px]">
+            {enyeFormat(data[1]?.[CR_Name]) || <span className="opacity-0">No data</span>}
+          </p>
+        </div>
+        <div className="w-[137.95275591px]">
+          <p className="pl-[12.57480315px]">
+            {data[1]?.[CR_TIN] || <span className="opacity-0">No data</span>}
+          </p>
+        </div>
+      </div>
+      <div className="flex mt-[9.8267716539px]">
+        <p className="ml-[312.36220472px]">
+          {data[1]?.[CR_Address] || <span className="opacity-0">No data</span>}
+        </p>
+      </div>
+      <div className="flex mt-[6.8267716539px]">
+        <p className="ml-[307.36220472px]">
+          {data[1]?.[CR_BusinessStyle] || (
+            <span className="opacity-0">No data</span>
+          )}
+        </p>
+      </div>
+      <div className="flex mt-[3.8267716539px]">
+        <p className="ml-[245.77165354px]">
+          {data[1]?.[CR_AmountInWords] || (
+            <span className="opacity-0">No data</span>
+          )}
+        </p>
+      </div>
+      <div className="flex w-full mt-[6.8267716539px]">
+        <div className="w-[325.03937008px]">
+          <p className="ml-[254.66929134px]">
+            {FormattedNumber(data[1]?.[CR_AmountInFigures]) || (
+              <span className="opacity-0">No data</span>
+            )}
+          </p>
+        </div>
+        <div className="w-[274.01574803px] ml-[158.28346457px]">
+          <p>
+            {data[1]?.[CR_Memo].substring(0, 35) || (
+              <span className="opacity-0">No data</span>
+            )}
+          </p>
+        </div>
+      </div>
+      <div className="ml-[245.77165354px] mt-[4.1811023622]">
+        <p>
+          {data[1]?.[CR_Memo].substring(35) || (
+            <span className="opacity-0">No data</span>
+          )}
+        </p>
+      </div>
+      <div className="w-[718.11023622px] pl-[566.92913386px] mt-[27.7952755906px]">
+        <p>
+          {data[1]?.[CR_PartnerName] || (
+            <span className="opacity-0">No Data</span>
+          )}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DSMToledo2;
