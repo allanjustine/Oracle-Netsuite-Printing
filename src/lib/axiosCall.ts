be0a090891ebe3056fd1908ab771b12b2945dc99
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-04-24 13:17:11 +0800

    getCsrfToken

--- src/lib/axiosCall.ts
+++ src/lib/axiosCall.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getCsrfToken } from "./authSanctum";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
@@ -10,27 +11,15 @@ const api = axios.create({
   },
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use(async (config) => {
   if (
     ["post", "put", "patch", "delete"].includes(
       config.method?.toLowerCase() ?? ""
     )
   ) {
-    const token = getCookie("XSRF-TOKEN");
-    if (token) {
-      config.headers["X-XSRF-TOKEN"] = token;
-    }
+    await getCsrfToken();
   }
   return config;
 });
-
-function getCookie(name: string) {
-  if (typeof document === "undefined") return null;
-
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()?.split(";").shift();
-  return null;
-}
 
 export default api;
