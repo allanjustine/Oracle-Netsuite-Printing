17088074ff6f2d2f4547f7676aa770a83e52d73a
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-04-24 09:21:43 +0800

    axios removed the withcredentials

--- src/lib/axiosCall.ts
+++ src/lib/axiosCall.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  withCredentials: true,
-  withXSRFToken: true,
+  // withCredentials: true,
+  // withXSRFToken: true,
 });
 
 export default api;
