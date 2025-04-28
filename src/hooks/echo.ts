bf53374d09493e04e7fa2b4c2be9b2ff080e3be5
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-04-28 16:32:31 +0800

    ws path

--- src/hooks/echo.ts
+++ src/hooks/echo.ts
@@ -9,6 +9,7 @@ const pusherClient = new Pusher(process.env.NEXT_PUBLIC_REVERB_APP_KEY!, {
   wssPort: Number(process.env.NEXT_PUBLIC_REVERB_PORT),
   forceTLS: (process.env.NEXT_PUBLIC_REVERB_SCHEME ?? "https") === "https",
   enabledTransports: ["ws", "wss"],
+  wsPath: "/app",
 });
 
 const echo: any = new Echo({
