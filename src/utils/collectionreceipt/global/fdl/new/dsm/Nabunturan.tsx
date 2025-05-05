87d9c58dc92cf340d49e181104acc8354098f945
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-05-05 14:05:22 +0800

    no refrence and no cashier

--- src/utils/collectionreceipt/global/fdl/new/dsm/Nabunturan.tsx
+++ src/utils/collectionreceipt/global/fdl/new/dsm/Nabunturan.tsx
@@ -24,12 +24,12 @@ const Nabunturan = ({ data }: any) => {
 
   return (
     <div className="text-xs h-[400.62992126px] w-[767.24409449px]">
-      <div className="w-[120.94488189px] flex items-center mt-[83.9291338579px] ml-[602.83464567px] h-[21.921259843px]">
+      <div className="w-[117.16535433px] flex items-center mt-[83.9291338579px] ml-[604.72440945px] h-[21.921259843px]">
         <p className="text-xs ml-[11.338582677px]">
           {data[1]?.[CR_Date] || <span className="opacity-0">No data</span>}
         </p>
       </div>
-      <div className="h-[61.984251969px] mt-[37.039370079px] ml-[164.40944882px]">
+      <div className="h-[61.984251969px] mt-[37.039370079px] ml-[149.29133858px]">
         <p className="w-[563.1496063px] ml-[11.338582677px] h-[20.409448819px] flex items-center">
           {enyeFormat(data[1]?.[CR_Name]) || <span className="opacity-0">No data</span>}
         </p>
@@ -40,33 +40,46 @@ const Nabunturan = ({ data }: any) => {
           {data[1]?.[CR_Address] || <span className="opacity-0">No data</span>}
         </p>
       </div>
-      <div className="flex w-[699.59055118px] ml-[24.566929134px] mt-[34.37007874px]">
-        <div className="flex flex-col items-center w-[608.50393701px]">
-          <p className="w-[491.33858268px] h-[23.433070866px] ml-[88.818897638px]">
+      <div className="flex w-[680.31496063px] mx-[37.795275591px] mt-[34.37007874px]">
+        <div className="flex flex-col w-[568.81889764px]">
+          <p className="h-[23.433070866px] pl-[83.149606299px]">
             {data[1]?.[CR_Memo].substring(0, 92) || (
               <span className="opacity-0">No data</span>
             )}
           </p>
-          <p className="w-[574.48818898px] ml-[11.338582677px] h-[23.433070866px]">
+          <p className="pl-[56.692913386px] h-[23.433070866px]">
             {data[1]?.[CR_Memo].substring(92) || (
               <span className="opacity-0">No data</span>
             )}
           </p>
         </div>
-        <div className="w-[120.56692913px]">
-          <p className="text-xs text-center h-[23.433070866px] ml-[7.5590551181px]">
+        <div className="w-[113.38582677px]">
+          <p className="text-xs text-center h-[23.433070866px]">
             {FormattedNumber(data[1]?.[CR_AmountInFigures]) || (
               <span className="opacity-0">No data</span>
             )}
           </p>
         </div>
       </div>
-      <div className="ml-[275.90551181px] text-center mt-[19.566929134px]">
-        <p className="w-[166.2992126px]">
-          {data[1]?.[CR_PartnerName] || (
-            <span className="opacity-0">Test Cashier</span>
-          )}
-        </p>
+      <div className="flex w-[680.31496063px] mx-[37.795275591px]">
+        <div className="text-[11px] w-[568.81889764px] flex-none">
+          <p><span className="opacity-0">No data</span></p>
+          <p className="text-center w-[162.51968504px] ml-[260.78740157px]">
+            {data[1]?.[CR_PartnerName] || ""}
+          </p>
+        </div>
+        <div className="w-[113.38582677px]">
+          <p className="text-xs text-center h-[23.433070866px]">
+            {FormattedNumber(data[1]?.[CR_AmountInFigures]) || (
+              <span className="opacity-0">No data</span>
+            )}
+          </p>
+          <p className="text-xs text-center h-[23.433070866px]">
+            {data[1]?.[CR_Reference] || (
+              <span className="opacity-0">No data</span>
+            )}
+          </p>
+        </div>
       </div>
     </div>
   );
