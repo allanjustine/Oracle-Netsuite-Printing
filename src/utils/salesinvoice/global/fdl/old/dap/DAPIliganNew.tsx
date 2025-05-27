7425e88fbd9b3f9f8e85b8d68798c85cc78babcb
smctdev <168695959+smctdev@users.noreply.github.com>
2025-05-27 16:42:17 +0800

    update dap iligan

--- src/utils/salesinvoice/global/fdl/old/dap/DAPIliganNew.tsx
+++ src/utils/salesinvoice/global/fdl/old/dap/DAPIliganNew.tsx
@@ -112,7 +112,7 @@ const DAPIliganNew = ({ data }: any) => {
 
   return (
     <div className="text-xs h-[771.02362205px] w-[601.32283465px]">
-      <div className="flex h-[20.787401575px] mt-[137.57480315px]">
+      <div className="flex h-[20.787401575px] mt-[118.67716535px]">
         <p className="w-[374.17322835px] pl-[98.267716535px] h-[22.299212598px]">
           {data[1]?.[mainLineName]
             ?.replace(/Ã/g, "Ñ")
@@ -326,7 +326,7 @@ const DAPIliganNew = ({ data }: any) => {
         </table>
       </div>
       <div className="mx-[34.393700787px]">
-        <div className="mt-[10.559055118px] ml-[332.976377952px]">
+        <div className="mt-[26.559055118px] ml-[332.976377952px]">
           <p className="text-xs text-center">
             {data[1]?.[cashier]
               ?.replace(/Ã/g, "Ñ")
