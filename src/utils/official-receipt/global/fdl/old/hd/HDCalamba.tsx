d70a37fa73d2431bae1665d4d995396df4786f74
Mark Anthony Bulala <117882831+macmac-prog@users.noreply.github.com>
2025-08-30 16:28:32 +0800

    Update HDCalamba OR to CR

--- src/utils/official-receipt/global/fdl/old/hd/HDCalamba.tsx
+++ src/utils/official-receipt/global/fdl/old/hd/HDCalamba.tsx
@@ -1,8 +1,9 @@
 "use client";
+import { PrintPageProps } from "@/types/types";
 import FormattedNumber from "@/utils/FormattedNumber";
 import enyeFormat from "@/utils/enyeFormat";
 
-const HDCalamba = ({ data }: any) => {
+const HDCalamba: React.FC<PrintPageProps> = ({ data }) => {
   const CR_Date = 0;
   const CR_Name = 1;
   const CR_TIN = 2;
@@ -16,74 +17,71 @@ const HDCalamba = ({ data }: any) => {
   const CR_Reference = 10;
 
   return (
-    <div className="text-xs w-[778.58267717px] h-[396.8503937px] ml-[10px]">
-      <div className="flex mt-[115.38582677px]">
-        <p className="pl-[604.72440945px] w-[151.18110236px]">
+    <div className="text-xs w-[805.03937008px] h-[415.7480315px]">
+      <div className="flex mt-[110.1732283431px]">
+        <p className="pl-[620.07874016px] w-[143.62204724px]">
           {data[1]?.[CR_Date] || <span className="opacity-0">No data</span>}
         </p>
       </div>
-      <div className="flex mt-[19.7952755906px]">
-        <div className="w-[566.92913386px]">
-          <p className="pl-[317.48031496px]">
+      <div className="flex justify-between mt-[24.7952755906px]">
+        <div className="w-[574.48818898px]">
+          <p className="pl-[321.25984252px]">
             {enyeFormat(data[1]?.[CR_Name]) || <span className="opacity-0">No data</span>}
           </p>
         </div>
-        <div className="w-[151.18110236px]">
-          <p
-            className={`ml-[38.57480315px] ${
-              data[1]?.[CR_TIN]?.length > 15 ? "text-[10px]" : ""
-            }`}
-          >
+        <div className="w-[207.87401575px]">
+          <p className="pl-[40.133858268px]">
             {data[1]?.[CR_TIN] || <span className="opacity-0">No data</span>}
           </p>
         </div>
       </div>
-      <div className="flex mt-[9.8267716539px]">
-        <p className="pl-[302.36220472px]">
+      <div className="flex mt-[6.8267716539px]">
+        <p className="pl-[313.7007874px]">
           {data[1]?.[CR_Address] || <span className="opacity-0">No data</span>}
         </p>
       </div>
-      <div className="flex mt-[6.8267716539px]">
-        <p className="ml-[302.36220472px]">
+      <div className="flex mt-[3.8267716539px]">
+        <p className="ml-[306.14173228px]">
           {data[1]?.[CR_BusinessStyle] || (
             <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
-      <div className="flex mt-[3.8267716539px]">
-        <p className="ml-[226.77165354px]">
-          {data[1]?.[CR_AmountInWords] || (
-            <span className="opacity-0">No data</span>
-          )}
-        </p>
-      </div>
-      <div className="flex w-full mt-[6.8267716539px]">
-        <div className="w-[325.03937008px]">
-          <p className="ml-[245.66929134px]">
+      <div className="flex w-full mt-[3.8267716539px]">
+        <div className="w-[600.94488189px]">
+          <p className="pl-[249.4488189px]">
+            {data[1]?.[CR_AmountInWords] || (
+              <span className="opacity-0">No data</span>
+            )}
+          </p>
+        </div>
+        <div className="w-[181.41732283px]">
+          <p className="pl-[52.913385827px]">
             {FormattedNumber(data[1]?.[CR_AmountInFigures]) || (
               <span className="opacity-0">No data</span>
             )}
           </p>
         </div>
-        <div className="w-[274.01574803px] ml-[132.28346457px]">
-          <p>
-            {data[1]?.[CR_Memo].substring(0, 35) || (
-              <span className="opacity-0">No data</span>
-            )}
-          </p>
-        </div>
       </div>
-      <div className="ml-[226.77165354px] mt-[4.1811023622]">
-        <p>
-          {data[1]?.[CR_Memo].substring(35) || (
+      <div className="w-[782.36220472px] mt-[7.8267716539px]">
+        <p className="w-[419.52755906px] ml-[347.71653543px]">
+          {data[1]?.[CR_Memo].substring(0, 70) || (
             <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
-      <div className="w-[718.11023622px] pl-[566.92913386px] mt-[30.7952755906px]">
-        <p>
+      <div className="w-[782.36220472px] mt-[6.8267716539px]">
+        <p className="w-[521.57480315px] ml-[245.66929134px]">
+          {data[1]?.[CR_Memo].substring(70) || (
+            <span className="opacity-0">No data</span>
+          )}
+        </p>
+      </div>
+
+      <div className="w-[782.36220472px] pl-[566.92913386px] mt-[28.7952755906px] text-[11px]">
+        <p className="text-center w-[170.07874016px]">
           {data[1]?.[CR_PartnerName] || (
-            <span className="opacity-0">No Data</span>
+            <span className="opacity-0">No data</span>
           )}
         </p>
       </div>
