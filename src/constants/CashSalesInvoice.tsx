e22aa79571f680e351615b4562343d9dba2a10e4
smctdev <168695959+smctdev@users.noreply.github.com>
2025-10-17 14:30:27 +0800

    updated si of anti

--- src/constants/CashSalesInvoice.tsx
+++ src/constants/CashSalesInvoice.tsx
@@ -499,8 +499,8 @@ const CashSalesInvoice: React.FC<PrintPageProps> = ({ data }) => {
     case "SILA" === user?.branchCode:
       return <SMCTSilang data={data} />;
     case "ANTI" === user?.branchCode:
-      return <SMCTAntique data={data} />; // old
-      // return <SmctAntique data={data} />; // new
+      // return <SMCTAntique data={data} />; // old
+      return <SmctAntique data={data} />; // new
     case "DSMT" === user?.branchCode:
       return <DSMTagbilaran data={data} />;
     case "DSMK" === user?.branchCode:
