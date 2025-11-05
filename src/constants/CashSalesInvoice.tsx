375d66d03b8636d42a1b147d456acc042883ec94
smctdev <168695959+smctdev@users.noreply.github.com>
2025-11-05 13:43:19 +0800

    updated si of kiba

--- src/constants/CashSalesInvoice.tsx
+++ src/constants/CashSalesInvoice.tsx
@@ -179,7 +179,7 @@ const CashSalesInvoice: React.FC<PrintPageProps> = ({ data }) => {
     // "KABAC",
     "LABA",
     "HO-DAP",
-    "KIBA",
+    "KIBAL",
   ];
 
   const dapCsiSizeData = ["DIPD", "JIME", "MANO"];
@@ -223,6 +223,7 @@ const CashSalesInvoice: React.FC<PrintPageProps> = ({ data }) => {
     "SIND",
     "HO-SMCT",
     "HO-DSM",
+    "KIBA",
   ];
 
   const fdlSiNewSizeData = [
