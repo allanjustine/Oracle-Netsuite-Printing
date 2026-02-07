4f7090abff69a370a4572fc90d1075920de27bfd
smctdev <168695959+smctdev@users.noreply.github.com>
2026-02-07 15:21:07 +0800

    updated cr of DSMTG

--- src/constants/CollectionReceipt.tsx
+++ src/constants/CollectionReceipt.tsx
@@ -291,6 +291,7 @@ const CollectionReceipt: React.FC<PrintPageProps> = ({ data }) => {
     "MALA",
     "DONC",
     "DSMV",
+    "DSMTG",
   ];
 
   const obbusCrOldSizeData = [
@@ -535,8 +536,8 @@ const CollectionReceipt: React.FC<PrintPageProps> = ({ data }) => {
       return <DSMCarmenCebu data={data} />; //OR
     // case "ANTI" === user?.branchCode:
     //   return <SMCTAntique data={data} />; //OR
-    case "DSMTG" === user?.branchCode:
-      return <Tagum data={data} />;
+    // case "DSMTG" === user?.branchCode:
+    //   return <Tagum data={data} />;
     case "SLIL" === user?.branchCode:
       return <SMCTLiloan data={data} />; //OR
     case "BUUD" === user?.branchCode:
