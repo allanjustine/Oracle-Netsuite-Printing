d66955e9f5ccafb344e9a331617e1906c30504bf
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-03-03 13:50:47 +0800

    loading

--- src/app/admin/_components/dashboard.tsx
+++ src/app/admin/_components/dashboard.tsx
@@ -100,7 +100,7 @@ export default function Dashboard() {
       <div className="flex-1">
         <Navbar />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
-          {loading ? (
+          {loadingDashboardData ? (
             <CardsLoader />
           ) : (
             <CardsCountList receiptRecords={receiptDashboards} />
