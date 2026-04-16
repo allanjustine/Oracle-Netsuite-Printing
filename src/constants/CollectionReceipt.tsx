2e1f8b8ab5c301317ae9db0b96e05669cf91428e
smctdev <168695959+smctdev@users.noreply.github.com>
2026-04-16 11:44:43 +0800

    updated cr of DSMDM and si of OZAD

--- src/constants/CollectionReceipt.tsx
+++ src/constants/CollectionReceipt.tsx
@@ -294,6 +294,7 @@ const CollectionReceipt: React.FC<PrintPageProps> = ({ data }) => {
     "OZAH",
     "LAPU",
     // "TUBI",
+    "DSMDM",
   ];
 
   const fdlOrNewSizeData = [];
@@ -595,8 +596,8 @@ const CollectionReceipt: React.FC<PrintPageProps> = ({ data }) => {
       return <SMCTCatarman data={data} />;
     case "SARG" === user?.branchCode:
       return <DSMSARG data={data} />;
-    case "DSMDM" === user?.branchCode:
-      return <DSMToledo2 data={data} />;
+    // case "DSMDM" === user?.branchCode:
+    //   return <DSMToledo2 data={data} />;
     case "CATAR2" === user?.branchCode:
       return <DSMCatarman2 data={data} />;
     case "GINGO" === user?.branchCode:
